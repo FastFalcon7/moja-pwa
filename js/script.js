bc6bc3f52fda5985e796d39ecfadb077b691c7e5
FastFalcon7 <mihalcin@hotmail.com>
2024-12-12 18:55:27 +0100

    oprava tlacitka

--- js/script.js
+++ js/script.js
@@ -7,15 +7,13 @@ if ('serviceWorker' in navigator) {
 // Pridanie event listenera na tlačidlo "Ďakujem"
 const thankYouButton = document.getElementById('thankYouButton');
 thankYouButton.addEventListener('click', () => {
-  // Vymaže celý obsah tela stránky
   document.body.innerHTML = `
     <h1>... a teraz klikni na tlačidlo Restart pre restartovanie aplikacie.</h1>
     <button id="restartButton">Restart</button>
   `;
   
   // Pridanie event listenera na tlačidlo "Restart"
-  const restartButton = document.getElementById('restartButton');
-  restartButton.addEventListener('click', () => {
-    window.location.reload(); // Reštartuje aplikáciu
+  document.getElementById('restartButton').addEventListener('click', () => {
+    window.location.reload();
   });
 });
