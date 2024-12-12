1d0c92a9b91ff2b0ea7a2fb6dd4d041e2ef3a0ca
FastFalcon7 <mihalcin@hotmail.com>
2024-12-12 18:41:06 +0100

    doplnenie tlacitka Restart

--- js/script.js
+++ js/script.js
@@ -7,5 +7,15 @@ if ('serviceWorker' in navigator) {
 // Pridanie event listenera na tlačidlo "Ďakujem"
 const thankYouButton = document.getElementById('thankYouButton');
 thankYouButton.addEventListener('click', () => {
-  document.body.innerHTML = ''; // Vymaže celý obsah tela stránky
+  // Vymaže celý obsah tela stránky
+  document.body.innerHTML = `
+    <h1>... a teraz klikni na tlačidlo Restart pre restartovanie aplikacie.</h1>
+    <button id="restartButton">Restart</button>
+  `;
+  
+  // Pridanie event listenera na tlačidlo "Restart"
+  const restartButton = document.getElementById('restartButton');
+  restartButton.addEventListener('click', () => {
+    window.location.reload(); // Reštartuje aplikáciu
+  });
 });
